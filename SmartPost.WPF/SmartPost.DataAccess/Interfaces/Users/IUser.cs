a073762725504d21f6c15a52360d57efc06e9dc7
SamandarbekYr <SamandarbekYR@gmail.com>
2024-01-24 20:08:30 +0500

    Delete WPF

---
+++ SmartPost.WPF/SmartPost.DataAccess/Interfaces/Users/IUser.cs
@@ -0,0 +1,12 @@
+﻿using SmartPost.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPost.DataAccess.Interfaces.Users
+{
+    public interface IUser : IRepository<User>
+    { }
+}
