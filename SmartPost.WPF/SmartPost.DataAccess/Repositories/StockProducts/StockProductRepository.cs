9e0113ea93f7297a23a28d871af291ea2cbfb297
Dilshodov01 <dilshodovziyodillo@gmail.com>
2024-01-27 06:37:29 +0500

    Brand Repository implemented

---
+++ SmartPost.WPF/SmartPost.DataAccess/Repositories/StockProducts/StockProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using SmartPost.DataAccess.Data;
+using SmartPost.DataAccess.Interfaces.StockProducts;
+using SmartPost.Domain.Entities.StokProducts;
+
+namespace SmartPost.DataAccess.Repositories.StockProducts;
+
+public class StockProductRepository : Repository<StokProduct>, IStockProductRepository
+{
+    public StockProductRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
