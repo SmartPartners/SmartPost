6c46c7a69022195b58acc55825b40d36d3033aa5
Dilshodov01 <dilshodovziyodillo@gmail.com>
2024-01-27 06:30:15 +0500

    Product and StockProduct Entities Added

---
+++ SmartPost.WPF/SmartPost.Service/Services/Products/ProductService.cs
@@ -0,0 +1,142 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SmartPost.Service.DTOs.Products;
+using SmartPost.Service.Commons.Exceptions;
+using SmartPost.Service.Interfaces.Products;
+using SmartPost.DataAccess.Interfaces.Barnds;
+using SmartPost.DataAccess.Interfaces.Products;
+using SmartPost.Domain.Entities.StorageProducts;
+using SmartPost.DataAccess.Interfaces.Categories;
+
+namespace SmartPost.Service.Services.Products;
+
+public class ProductService : IProductService
+{
+    private readonly IMapper _mapper;
+    private readonly IBrandRepository _brandRepository;
+    private readonly IProductRepository _productRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ProductService(IMapper mapper, IProductRepository productRepository, ICategoryRepository categoryRepository, IBrandRepository brandRepository)
+    {
+        this._mapper = mapper;
+        this._productRepository = productRepository;
+        this._categoryRepository = categoryRepository;
+        _brandRepository = brandRepository;
+    }
+
+    public async Task<ProductForResultDto> CreateAsync(ProductForCreationDto productForCreationDto)
+    {
+        var category = await _categoryRepository.SelectAll()
+           .Where(c => c.Id == productForCreationDto.CategoryId)
+           .AsNoTracking()
+           .FirstOrDefaultAsync();
+
+        if (category is null)
+            throw new CustomException(404, "Category is not found");
+
+        var brand = await _brandRepository.SelectAll()
+             .Where(b => b.Id == productForCreationDto.BrandId)
+             .FirstOrDefaultAsync();
+
+        if (brand is null)
+            throw new CustomException(404, "Brand is not found");
+
+        var product = await _productRepository.SelectAll()
+            .Where(p => p.ProductName == productForCreationDto.ProductName)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+
+        if (product is null )
+        {
+            var mappedProduct = _mapper.Map<Product>(productForCreationDto);
+            mappedProduct.CreatedAt = DateTime.UtcNow;
+            return _mapper.Map<ProductForResultDto>(await _productRepository.InsertAsync(mappedProduct));
+        }
+
+        product.Quantity += productForCreationDto.Quantity;
+        return _mapper.Map<ProductForResultDto>(await _productRepository.UpdateAsync(product));
+
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        var product = await _productRepository.SelectAll()
+            .Where(p => p.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (product is null)
+            throw new CustomException(404, "Product is not found");
+
+        return await _productRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<ProductForResultDto>> GetAllAsync()
+       => _mapper.Map<IEnumerable<ProductForResultDto>>(await _productRepository.SelectAll().AsNoTracking().ToListAsync());
+
+
+
+    public async Task<ProductForResultDto> GetByIdAsync(long id)
+    {
+        var product = await _productRepository.SelectAll()
+             .Where(p => p.Id == id)
+             .Include(p=>p.Brand)
+             .Include(p=>p.Category)
+             .AsNoTracking()
+             .FirstOrDefaultAsync();
+
+        if (product is null)
+            throw new CustomException(404, "Product is not found");
+
+        return _mapper.Map<ProductForResultDto>(product);
+    }
+
+
+    public async Task<ProductForResultDto> UpdateAsync(ProductForUpdateDto productForUpdateDto)
+    {
+        var category = await _categoryRepository.SelectAll()
+           .Where(c => c.Id == productForUpdateDto.CategoryId)
+           .AsNoTracking()
+           .FirstOrDefaultAsync();
+
+        if (category is null)
+            throw new CustomException(404, "Category is not found");
+
+        var brand = await _brandRepository.SelectAll()
+             .Where(b => b.Id == productForUpdateDto.BrandId)
+             .FirstOrDefaultAsync();
+
+        if (brand is null)
+            throw new CustomException(404, "Brand is not found");
+
+        var product = await _productRepository.SelectAll()
+             .Where(p => p.Id == productForUpdateDto.Id)
+             .AsNoTracking()
+             .FirstOrDefaultAsync();
+
+        if (product is null)
+            throw new CustomException(404, "Product is not found");
+
+        var mappedProduct = _mapper.Map<Product>(productForUpdateDto);
+        mappedProduct.UpdatedAt = DateTime.UtcNow;
+
+        return _mapper.Map<ProductForResultDto>(await _productRepository.UpdateAsync(mappedProduct));
+    }
+
+    public async Task<ProductForResultDto> GetByName(string name)
+    {
+        var Product =await _productRepository.SelectAll()
+            .Where(p=>p.ProductName == name)
+            .Include(s=>s.Brand)
+            .Include(s=>s.Category)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (Product is null)
+            throw new CustomException(404, "Product is not found");
+
+        return _mapper.Map<ProductForResultDto>(Product);
+    }
+}
---
+++ SmartPost.WPF/SmartPost.Service/Services/StockProducts/StockProductService.cs
@@ -0,0 +1,140 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SmartPost.DataAccess.Interfaces.Barnds;
+using SmartPost.DataAccess.Interfaces.Categories;
+using SmartPost.DataAccess.Interfaces.StockProducts;
+using SmartPost.Domain.Entities.StokProducts;
+using SmartPost.Service.Commons.Exceptions;
+using SmartPost.Service.DTOs.StockProducts;
+using SmartPost.Service.Interfaces.StockProducts;
+
+namespace SmartPost.Service.Services.StockProducts;
+
+public class StockProductService : IStockProductService
+{
+    private readonly IMapper _mapper;
+    private readonly IBrandRepository _brandRepository;
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IStockProductRepository _stockProductRepository; 
+
+
+    public StockProductService(IMapper mapper, IStockProductRepository stockProductRepository, IBrandRepository brandRepository, ICategoryRepository categoryRepository)
+    {
+        _mapper = mapper;
+        _brandRepository = brandRepository;
+        _categoryRepository = categoryRepository;
+        _stockProductRepository = stockProductRepository;
+    }
+
+    public async Task<StockProductsForResultDto> CreateAsync(StockProductForCreationDto createDto)
+    {
+
+        var category = await _categoryRepository.SelectAll()
+           .Where(c => c.Id == createDto.CategoryId)
+           .AsNoTracking()
+           .FirstOrDefaultAsync();
+
+        if (category is null)
+            throw new CustomException(404, "Category is not found");
+
+        var brand = await _brandRepository.SelectAll()
+             .Where(b => b.Id == createDto.BrandId)
+        .FirstOrDefaultAsync();
+
+        if (brand is null)
+            throw new CustomException(404, "Brand is not found");
+        
+        var stockProduct = await _stockProductRepository.SelectAll()
+            .Where(s => s.PCode == createDto.PCode || s.ProductName == createDto.ProductName || s.BarCode == createDto.BarCode)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (stockProduct is null)
+        {
+            var mappedStockProduct = _mapper.Map<StokProduct>(createDto);
+            mappedStockProduct.CreatedAt = DateTime.UtcNow;
+            return _mapper.Map<StockProductsForResultDto>(await _stockProductRepository.InsertAsync(mappedStockProduct));
+        }
+
+        stockProduct.Quantity += createDto.Quantity;
+        return _mapper.Map<StockProductsForResultDto>(await _stockProductRepository.UpdateAsync(stockProduct));
+    }
+
+    public async Task<bool> DeleteAsymc(long id)
+    {
+        var StockProduct = await _stockProductRepository.SelectAll()
+            .Where(s=>s.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (StockProduct is null)
+            throw new CustomException(404, "StockProduct is not found");
+
+         return await _stockProductRepository.DeleteAsync(id);
+    }
+       
+
+    public async Task<IEnumerable<StockProductsForResultDto>> GetAllAsync()
+        => _mapper.Map<IEnumerable<StockProductsForResultDto>>(await _stockProductRepository.SelectAll().AsNoTracking().ToListAsync());
+   
+
+    public async Task<StockProductsForResultDto> GetByIdAsync(long id)
+    {
+        var StockProduct = await _stockProductRepository.SelectAll()
+           .Where(s => s.Id == id)
+           .AsNoTracking()
+           .FirstOrDefaultAsync();
+
+        if (StockProduct is null)
+            throw new CustomException(404, "StockProduct is not found");
+
+        return _mapper.Map<StockProductsForResultDto>(StockProduct);
+    }
+   
+
+    public async Task<StockProductsForResultDto> UpdateAsync(StockProductForUpdateDto updateDto)
+    {
+
+        var category = await _categoryRepository.SelectAll()
+          .Where(c => c.Id == updateDto.CategoryId)
+          .AsNoTracking()
+          .FirstOrDefaultAsync();
+
+        if (category is null)
+            throw new CustomException(404, "Category is not found");
+
+        var brand = await _brandRepository.SelectAll()
+             .Where(b => b.Id == updateDto.BrandId)
+             .FirstOrDefaultAsync();
+
+        if (brand is null)
+            throw new CustomException(404, "Brand is not found");
+
+        var stockProduct = await _stockProductRepository.SelectAll()
+            .Where(s => s.Id == updateDto.Id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (stockProduct is null)
+            throw new CustomException(404, "StockProduct is not found");
+
+        var mappedStockProduct = _mapper.Map<StokProduct>(updateDto);
+        mappedStockProduct.UpdatedAt = DateTime.UtcNow;
+
+        return _mapper.Map<StockProductsForResultDto>(await _stockProductRepository.UpdateAsync(mappedStockProduct));
+    }
+
+    public async Task<StockProductsForResultDto>AddQuentityToStockProduct(long id, decimal quantity)
+    {
+        var stockProduct = await _stockProductRepository.SelectAll()
+            .Where(s => s.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (stockProduct is null)
+            throw new CustomException(404, "StockProduct is not found");
+
+        stockProduct.Quantity += quantity;
+        return _mapper.Map<StockProductsForResultDto>(await _stockProductRepository.UpdateAsync(stockProduct));
+    }
+}
